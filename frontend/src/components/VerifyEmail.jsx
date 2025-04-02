dba6813aa335a08416cb0f64ad4bc3c8f5666bbd
Ankur Saini <sainiankur0508@gmail.com>
2025-04-02 17:44:05 +0530

    Update VerifyEmail.jsx

--- frontend/src/components/VerifyEmail.jsx
+++ frontend/src/components/VerifyEmail.jsx
@@ -8,7 +8,7 @@ const VerifyEmail = () => {
   useEffect(() => {
     const verifyEmail = async () => {
       try {
-        const response = await fetch(`http://localhost:5000/api/auth/verify-email/${token}`);
+        const response = await fetch(`https://hosting-ra5z.vercel.app/api/auth/verify-email/${token}`);
         
         if (response.ok) {
           alert("Email verified successfully! Please log in.");
